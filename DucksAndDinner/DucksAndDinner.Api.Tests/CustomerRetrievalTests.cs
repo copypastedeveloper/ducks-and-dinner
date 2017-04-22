93e2dcc97abca88c01e45115f1ad3d293e0abc39
Nathan Gonzalez <ncgonz@gmail.com>
2017-04-22 13:50:14 -0500

    oops

---
+++ DucksAndDinner/DucksAndDinner.Api.Tests/CustomerRetrievalTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DucksAndDinner.Api.Controllers;
+using DucksAndDinner.Api.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace DucksAndDinner.Api.Tests
+{
+    [TestClass]
+    public class CustomerRetrievalTests
+    {
+        Mock<ICustomerRepository> _mockedCustomerRepository;
+        CustomerController _controller;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockedCustomerRepository = new Mock<ICustomerRepository>();
+
+            _controller = new CustomerController(_mockedCustomerRepository.Object);
+            _controller.Request = new HttpRequestMessage();
+            _controller.Request.SetConfiguration(new HttpConfiguration());
+        }
+
+        [TestMethod]
+        public void GetAllCustomersShouldReturnAllCustomers()
+        {
+            //arrange
+            _mockedCustomerRepository.Setup(x => x.GetAll())
+                .Returns(() =>
+                    new List<Customer>
+                    {
+                        new Customer {UserName = "Jimmy"},
+                        new Customer {UserName = "Steve"},
+                        new Customer {UserName = "Sally"}
+                    });
+
+            //act
+            var result = _controller.GetAll();
+
+            //assert
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+            var content = result.Content as ObjectContent<IEnumerable<Customer>>;
+            var returnValue = content.Value as IEnumerable<Customer>;
+            Assert.AreEqual(3, returnValue.Count());
+        }
+    }
+}
