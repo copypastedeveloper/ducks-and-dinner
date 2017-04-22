636cf76e1ed8ff57a25356157e877c851a31132b
Nathan Gonzalez <ncgonz@gmail.com>
2017-04-22 13:16:35 -0500

    added repository implementation and now everything is saving all the way through

---
+++ DucksAndDinner/DucksAndDinner.Api/Controllers/ICustomerRepository.cs
@@ -0,0 +1,9 @@
+﻿using DucksAndDinner.Api.Models;
+
+namespace DucksAndDinner.Api.Controllers
+{
+    public interface ICustomerRepository
+    {
+        void Save(Customer newCustomer);
+    }
+}
---
+++ DucksAndDinner/DucksAndDinner.Api/DAL/CustomerRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using Dapper;
+using DucksAndDinner.Api.Controllers;
+using DucksAndDinner.Api.Models;
+
+namespace DucksAndDinner.Api.DAL
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        readonly IDbConnection _dbConnection;
+
+        public CustomerRepository(IDbConnection connection)
+        {
+            _dbConnection = connection;
+        }
+
+        public void Save(Customer newCustomer)
+        {
+            var sql = @"Insert into Customer(username,firstname,lastname,password,numberofduckspertypicalmeal)
+                        Values(@username,@firstname,@lastname,@password,@numberofduckspertypicalmeal)";
+
+            _dbConnection.Execute(sql, newCustomer);
+        }
+    }
+}
