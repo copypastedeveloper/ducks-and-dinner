cc8a64de502cedc1502688a00d395078041f287f
Nathan Gonzalez <ncgonz@gmail.com>
2017-05-06 13:10:16 -0500

    added entity framework and a duck

---
+++ DucksAndDinner/DucksAndDinner.Api/DAL/AppContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using DucksAndDinner.Api.Models;
+
+namespace DucksAndDinner.Api.DAL
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Duck> Ducks { get; set; }
+    }
+}
---
+++ DucksAndDinner/DucksAndDinner.Api/Models/Duck.cs
@@ -0,0 +1,9 @@
+﻿namespace DucksAndDinner.Api.Models
+{
+    public class Duck
+    {
+        public string Name { get; set; }
+        public string Picture { get; set; }
+        public int Weight { get; set; }
+    }
+}
