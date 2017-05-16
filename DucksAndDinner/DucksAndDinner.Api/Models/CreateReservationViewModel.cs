ddeb8ccf411a8e1d71669528976232cd3b23d1b5
Nathan Gonzalez <ncgonz@gmail.com>
2017-05-15 20:26:17 -0500

    adding annotations

--- DucksAndDinner/DucksAndDinner.Api/Models/CreateReservationViewModel.cs
+++ DucksAndDinner/DucksAndDinner.Api/Models/CreateReservationViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,8 +9,11 @@ namespace DucksAndDinner.Api.Models
 {
     public class CreateReservationViewModel
     {
+        [DisplayName("Reservation Date")]
         public DateTime Date { get; set; }
+        [DisplayName("Number of People Eating Duck")]
         public int NumberOfPeople { get; set; }
+        [DisplayName("Which Duck To Eat")]
         public int DuckId { get; set; }
 
         public List<SelectListItem> Ducks { get; set; }
