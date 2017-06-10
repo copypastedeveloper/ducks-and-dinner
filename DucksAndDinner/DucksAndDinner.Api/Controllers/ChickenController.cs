410fdf53148925a1db196cbf8f6f52d89033002b
Nathan Gonzalez <nathan@copypastedeveloper.com>
2017-06-10 11:10:25 -0500

    communication with external api

---
+++ DucksAndDinner/DucksAndDinner.Api/Controllers/ChickenController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+using DucksAndDinner.Api.DAL;
+using RestSharp;
+
+namespace DucksAndDinner.Api.Controllers
+{
+    public class ChickenController : ApiController
+    {
+        readonly IPermitRepository _permitRepository;
+
+        public ChickenController(IPermitRepository permitRepository)
+        {
+            _permitRepository = permitRepository;
+        }
+
+        public IEnumerable<Permit> GetPermits()
+        {
+            return _permitRepository.Get();
+        }
+    }
+
+    public class ApiPermit
+    {
+        public string mapped_location_city { get; set; }
+        public string mapped_location_state { get; set; }
+        public string mapped_location_zip { get; set; }
+        public string permit { get; set; }
+    }
+
+    public class Permit
+    {
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string PermitNumber { get; set; }
+    }
+}
---
+++ DucksAndDinner/DucksAndDinner.Api/DAL/PermitRepository.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using DucksAndDinner.Api.Controllers;
+using RestSharp;
+
+namespace DucksAndDinner.Api.DAL
+{
+    public class PermitRepository : IPermitRepository
+    {
+        public IEnumerable<Permit> Get()
+        {
+            var client = new RestClient("http://data.nashville.gov/");
+            var request = new RestRequest($"resource/8fb8-y573.json");
+
+            request.AddHeader("X-App-Token", "");
+            request.AddQueryParameter("myParam", "jkasdfjlk;adsfjk;lasfd;kjl");
+
+            var restResponse = client.Get<List<ApiPermit>>(request);
+
+            if (restResponse.StatusCode != HttpStatusCode.OK) yield return null;
+
+            foreach (var permit in restResponse.Data)
+            {
+                var p = new Permit
+                {
+                    PermitNumber = permit.permit,
+                    City = permit.mapped_location_city,
+                    State = permit.mapped_location_state,
+                    ZipCode = permit.mapped_location_zip
+                };
+
+                yield return p;
+            }
+        }
+    }
+
+    public interface IPermitRepository
+    {
+        IEnumerable<Permit> Get();
+    }
+}
